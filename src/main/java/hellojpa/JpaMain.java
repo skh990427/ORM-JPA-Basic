e7cf8fb09c9b6cb392e32e9444f96dad68699e85
KwangHo <skh990427@naver.com>
2024-01-30 20:47:54 +0900

    feat 멤버 저장

--- src/main/java/hellojpa/JpaMain.java
+++ src/main/java/hellojpa/JpaMain.java
@@ -8,9 +8,27 @@ public class JpaMain {
 
         EntityManagerFactory emf = Persistence.createEntityManagerFactory("hello");
         EntityManager em = emf.createEntityManager();
-        //code
 
-        em.close();
+        /**
+         * 모든 데이터의 변경은 트랜잭션 안에서 변경되어야함
+         */
+        EntityTransaction tx = em.getTransaction();
+        tx.begin();
+
+        try {
+            //code
+            Member member = new Member();
+            member.setId(2L);
+            member.setName("HelloB");
+            em.persist(member);
+
+            tx.commit();
+        } catch (Exception e) {
+            tx.rollback();
+        } finally {
+            em.close();
+        }
+
         emf.close();
     }
 }
