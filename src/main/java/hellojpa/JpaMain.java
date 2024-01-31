7914bb95419e013310265c94ae5533dfc39851e0
KwangHo <skh990427@naver.com>
2024-01-31 20:29:06 +0900

    영속성 컨텍스트 - 변경 감지(dirty Checking)

--- src/main/java/hellojpa/JpaMain.java
+++ src/main/java/hellojpa/JpaMain.java
@@ -18,13 +18,12 @@ public class JpaMain {
         tx.begin();
 
         try {
+            Member member = em.find(Member.class, 150L);
 
-            Member member1 = new Member(150L, "A");
-            Member member2 = new Member(160L, "B");
+            member.setName("ZZZZZ");
 
-            em.persist(member1);
-            em.persist(member2);
-            System.out.println("=============================="); //이 전에 쿼리가 나가는지 이 이후에 쿼리가 나가는지 보면된다
+            //JPA는 컬렉션처럼 운영 된다!
+            //em.persist(member); - persist 호출해야되는것 아니야?! 는 무슨 하핫! 변경 감지!
 
             tx.commit(); //트랜잭션이 커밋하는 시점에 영속성 컨텍스트에 있는 값이 날아가서 저장된다!
         } catch (Exception e) {
