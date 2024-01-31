f8a87354d753096269c7dc5c9969f8371dbf4e62
KwangHo <skh990427@naver.com>
2024-01-31 20:20:20 +0900

    영속성 컨텍스트 - 쓰기 지연(쿼리 모아서 DB에 날림)

--- src/main/java/hellojpa/JpaMain.java
+++ src/main/java/hellojpa/JpaMain.java
@@ -18,10 +18,13 @@ public class JpaMain {
         tx.begin();
 
         try {
-            Member findMember1 = em.find(Member.class, 101L);
-            Member findMember2 = em.find(Member.class, 101L);
 
-            System.out.println("result = " + (findMember1==findMember2));
+            Member member1 = new Member(150L, "A");
+            Member member2 = new Member(160L, "B");
+
+            em.persist(member1);
+            em.persist(member2);
+            System.out.println("=============================="); //이 전에 쿼리가 나가는지 이 이후에 쿼리가 나가는지 보면된다
 
             tx.commit(); //트랜잭션이 커밋하는 시점에 영속성 컨텍스트에 있는 값이 날아가서 저장된다!
         } catch (Exception e) {
