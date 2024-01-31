f2a0e077730966cade053d2bdf8d52f133622da1
KwangHo <skh990427@naver.com>
2024-01-31 20:01:25 +0900

    영속성 컨텍스트 - 1차캐시 셀렉트쿼리 안나가는데 조회

--- src/main/java/hellojpa/JpaMain.java
+++ src/main/java/hellojpa/JpaMain.java
@@ -21,17 +21,15 @@ public class JpaMain {
 
             //비영속 상태
             Member member = new Member();
-            member.setId(100L);
+            member.setId(101L);
             member.setName("HelloJPA");
 
             //영속 상태
             em.persist(member); //이때 DB에 저장되는것이 아님! 그냥 객체를 영속상태로 변경하는 것 뿐!
 
-            //준영속 상태
-            em.detach(member); //준영속 상태로변경! - 준영속이란? = 영속성 컨텍스트에서 다시 지움!
-
-            //실제 db 삭제를 요청하는 상태
-            em.remove(member);
+            Member findMember = em.find(Member.class, 101L);
+            System.out.println("findMember.id = " + findMember.getId());
+            System.out.println("findMember.name = " + findMember.getName());
 
             tx.commit(); //트랜잭션이 커밋하는 시점에 영속성 컨텍스트에 있는 값이 날아가서 저장된다!
         } catch (Exception e) {
