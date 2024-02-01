ef8646eb9dd7fb857022bd69fdadba90856228da
KwangHo <skh990427@naver.com>
2024-02-01 20:47:39 +0900

    필드 매핑 종류

--- src/main/java/hellojpa/JpaMain.java
+++ src/main/java/hellojpa/JpaMain.java
@@ -19,15 +19,7 @@ public class JpaMain {
 
         try {
 
-            //em.find로 찾아서 영속성 컨텐스트에 가져오면 영속상태가 된다.
-            Member member = em.find(Member.class, 150L);
-            member.setName("AAAAAA"); // - 변경 감지
-
-            em.detach(member); // 특정 엔티티만 준영속 상태로 전환
-            em.clear(); // 영속성 컨텍스트 안에 모든 엔티티를 준영속 상태로 전환(초기화)
-            em.close(); // 영속성 컨텍스트를 그냥 닫아버림
-
-            tx.commit(); //트랜잭션이 커밋하는 시점에 영속성 컨텍스트에 있는 값이 날아가서 저장된다!
+            tx.commit();
         } catch (Exception e) {
             tx.rollback();
         } finally {
--- src/main/java/hellojpa/Member.java
+++ src/main/java/hellojpa/Member.java
@@ -1,39 +1,36 @@
 package hellojpa;
 
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
+
+import java.util.Date;
 
 @Entity
 public class Member {
 
-    @Id
+    @Id //pk 매핑
     private Long id;
 
-    @Column(unique = true, length = 10) //유니크 필드인지, 길이가 10인지? 결정할 수 있다.
-    private String name;
+    @Column(name = "name") //객체는 username 으로 쓰고싶은데 db는 name으로 쓰고싶다면? 이름지정!
+    private String username;
+
+    private Integer age;
+
+    @Enumerated(EnumType.STRING) //Enum 타입은 DB에 없기때문에 이 애노테이션 사용, STRING 으로 비교해야 나중에 중간에 값 들어갔을때 오류 안남
+    private RoleType roleType;
+
+    @Temporal(TemporalType.TIMESTAMP) //이건 나중에 BaseEntity 만들어서 Auditing 사용하면 됨
+    private Date createdDate;
+
+    @Temporal(TemporalType.TIMESTAMP)
+    private Date lastModifiedDate;
+
+    @Lob //VARCHAR를 넘어서는.. 큰 컨텐츠..?
+    private String description;
+
+    @Transient //이 데이터는 db에 쓰고싶지 않아.. 그냥 메모리에서만 관리하고싶어! 할 때 쓰는 애노테이션
+    private int temp;
 
     protected Member() {
     }
 
-    public Member(Long id, String name) {
-        this.id = id;
-        this.name = name;
-    }
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
 }
