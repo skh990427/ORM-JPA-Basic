00ad373e1609a0b8de8672a072024f7924d5e463
KwangHo <skh990427@naver.com>
2024-02-02 17:18:18 +0900

    기본키 매핑

--- src/main/java/hellojpa/Member.java
+++ src/main/java/hellojpa/Member.java
@@ -8,29 +8,31 @@ import java.util.Date;
 public class Member {
 
     @Id //pk 매핑
+//    @GeneratedValue(strategy = GenerationType.AUTO) //기본값 DB 방언에 맞춰 자동으로 생성
+//    @GeneratedValue(strategy = GenerationType.IDENTITY) //데이터베이스에 아이디 생성 위임
+    @GeneratedValue(strategy = GenerationType.SEQUENCE) //오라클 같은곳에서 많이 사용하는 전략
+//    @GeneratedValue(strategy = GenerationType.TABLE)
     private Long id;
 
-    @Column(name = "name", insertable = true, updatable = false, nullable = false, unique = true, length = 100)
+    @Column(name = "name")
     private String username;
-
-    private Integer age;
-
-    @Enumerated(EnumType.STRING) //Enum 타입은 DB에 없기때문에 이 애노테이션 사용, STRING 으로 비교해야 나중에 중간에 값 들어갔을때 오류 안남
-    private RoleType roleType;
-
-    @Temporal(TemporalType.TIMESTAMP) //이건 나중에 BaseEntity 만들어서 Auditing 사용하면 됨
-    private Date createdDate;
-
-    @Temporal(TemporalType.TIMESTAMP)
-    private Date lastModifiedDate;
-
-    @Lob //VARCHAR를 넘어서는.. 큰 컨텐츠..?
-    private String description;
-
-    @Transient //이 데이터는 db에 쓰고싶지 않아.. 그냥 메모리에서만 관리하고싶어! 할 때 쓰는 애노테이션
-    private int temp;
 
     protected Member() {
     }
 
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
 }
