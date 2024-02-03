63c7d0e292df08122a573a2ac404999c1063f6ad
KwangHo <skh990427@naver.com>
2024-02-03 15:26:57 +0900

    실습예제는 프로젝트 새로파서 하자

---
+++ src/main/java/hellojpa/JpaMain.java
@@ -0,0 +1,34 @@
+package hellojpa;
+
+import jakarta.persistence.*;
+
+public class JpaMain {
+
+    public static void main(String[] args) {
+
+        EntityManagerFactory emf = Persistence.createEntityManagerFactory("hello");
+        EntityManager em = emf.createEntityManager();
+
+        /**
+         * 모든 데이터의 변경은 트랜잭션 안에서 변경되어야함
+         */
+        EntityTransaction tx = em.getTransaction();
+        tx.begin();
+
+        try {
+
+            Member member = new Member();
+            member.setUsername("C");
+
+            em.persist(member);
+            System.out.println("IDENTITY 는 이떄 insert 쿼리가 이미 날아감");
+            tx.commit();
+        } catch (Exception e) {
+            tx.rollback();
+        } finally {
+            em.close();
+        }
+
+        emf.close();
+    }
+}
