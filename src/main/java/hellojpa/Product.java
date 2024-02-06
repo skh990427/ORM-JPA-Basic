19897bb3a363a256bb2cde677cf8f548250f52df
KwangHo <skh990427@naver.com>
2024-02-06 19:45:43 +0900

    다양한 연관관계 - n:m 양방향 관계(1:n - n:1로 해결)

--- src/main/java/hellojpa/Product.java
+++ src/main/java/hellojpa/Product.java
@@ -13,8 +13,8 @@ public class Product {
 
     private String name;
 
-    @ManyToMany(mappedBy = "products")
-    private List<Member> members = new ArrayList<>();
+    @OneToMany(mappedBy = "product")
+    private List<MemberProduct> memberProducts = new ArrayList<>();
 
     public Long getId() {
         return id;
