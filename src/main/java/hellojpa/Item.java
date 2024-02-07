df05c24a6a6ab7bb9c9b06db46297c113e17bd0a
KwangHo <skh990427@naver.com>
2024-02-07 17:18:58 +0900

    고급 매핑 - 상속관계 매핑(싱글테이블 예시, 성능은 제일 좋음)

--- src/main/java/hellojpa/Item.java
+++ src/main/java/hellojpa/Item.java
@@ -3,8 +3,8 @@ package hellojpa;
 import jakarta.persistence.*;
 
 @Entity
-@Inheritance(strategy = InheritanceType.JOINED) //전략 선택, 기본값은 싱글 테이블
-@DiscriminatorColumn //DTYPE 컬럽 추가 이름은 바꿀 수 있는데 그냥 관례로 쓰는편이 좋음
+@Inheritance(strategy = InheritanceType.SINGLE_TABLE) //전략 선택, 기본값은 싱글 테이블
+@DiscriminatorColumn //DTYPE 컬럽 추가 이름은 바꿀 수 있는데 그냥 관례로 쓰는편이 좋음, 단인테이블 전략은 이 애노테이션 없어도 됨 DTYPE 없으면 싱글테이블전략은 이게 뭔지 알수가없어서 자동으로 추가됨
 public class Item {
 
     @Id @GeneratedValue
