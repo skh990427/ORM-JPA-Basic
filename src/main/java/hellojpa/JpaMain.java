edf3cc60baed8efbc20a3943ffc5129165fc83b0
KwangHo <skh990427@naver.com>
2024-02-14 20:21:39 +0900

    값 타입 - 값 타입을 공유하여 사용할 때 생기는 문제점

--- src/main/java/hellojpa/JpaMain.java
+++ src/main/java/hellojpa/JpaMain.java
@@ -20,12 +20,22 @@ public class JpaMain {
 
         try {
 
-            Member member = new Member();
-            member.setUsername("hello");
-            member.setHomeAddress(new Address("city", "street", "zipcode"));
-            member.setWorkPeriod(new Period());
+            Address address = new Address("city", "street", "10000");
 
-            em.persist(member);
+            Member member1 = new Member();
+            member1.setUsername("member1");
+            member1.setHomeAddress(address);
+            em.persist(member1);
+
+            Member member2 = new Member();
+            member2.setUsername("member2");
+            member2.setHomeAddress(address);
+            em.persist(member2);
+
+            // 저~~~기 멀리 아래같은 코드가 있음 첫번째 멤버만 바꾸고싶은데..? 돌려보면..? 이런 버그는 잡기 힘듬.. 아니 이걸 어캐잡아
+            // 값 타입은 여러 엔티티에서 공유하면 위험함. 그럼 1:1때만 써야 하는건가?
+
+            member1.getHomeAddress().setCity("newCity");
 
             tx.commit();
         } catch (Exception e) {
