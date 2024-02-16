04211c633f976cb6fcca6966e4690f91201c9f6a
KwangHo <skh990427@naver.com>
2024-02-16 18:16:29 +0900

    JPQL - 간단 예시(동적쿼리 문제)

--- src/main/java/hellojpa/JpaMain.java
+++ src/main/java/hellojpa/JpaMain.java
@@ -1,10 +1,8 @@
 package hellojpa;
 
 import jakarta.persistence.*;
-import org.hibernate.Hibernate;
 
 import java.util.List;
-import java.util.Set;
 
 public class JpaMain {
 
@@ -20,37 +18,12 @@ public class JpaMain {
         tx.begin();
 
         try {
-            Member member = new Member();
-            member.setUsername("member1");
-            member.setHomeAddress(new Address("homeCity", "street", "10000"));
+            List<Member> result = em.createQuery("select m from Member m where m.username like '%kim%'", Member.class)
+                    .getResultList();
 
-            member.getFavoriteFoods().add("치킨");
-            member.getFavoriteFoods().add("족발");
-            member.getFavoriteFoods().add("피자");
-
-            member.getAddressHistory().add(new AddressEntity("old1", "street", "10000"));
-            member.getAddressHistory().add(new AddressEntity("old2", "street", "10000"));
-
-            em.persist(member);
-
-            em.flush();
-            em.clear();
-
-            System.out.println("============== START ==============");
-            Member findMember = em.find(Member.class, member.getId());
-
-            Address a = findMember.getHomeAddress();
-            findMember.setHomeAddress(new Address("new City", a.getStreet(), a.getZipcode())); //값 타입은 통으로 갈아껴야함
-
-            //치킨 -> 한식 으로 변경
-            //얘는 변경 하는 방법이 없음 그냥 지우고 새로 넣어야함
-//            findMember.getFavoriteFoods().remove("치킨");
-//            findMember.getFavoriteFoods().add("한식");
-
-            //주소를 old1 -> new1
-            //remove 할때는 equals로 비교해서 지우는데 이럴떄 equals가 제대로 구현이 안되어있으면 망하는거임
-//            findMember.getAddressHistory().remove(new Address("old1", "street", "10000"));
-//            findMember.getAddressHistory().add(new Address("newCity1", "street", "10000"));
+            for (Member member : result) {
+                System.out.println("member = " + member);
+            }
 
             tx.commit();
         } catch (Exception e) {
